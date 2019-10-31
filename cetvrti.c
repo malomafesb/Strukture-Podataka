b3f21ac632db447f74ff4e81a24583d8cec9956f
martabatinic <56558228+martabatinic@users.noreply.github.com>
2019-10-31 17:42:18 +0100

    Create cetvrti.c

---
+++ cetvrti.c
@@ -0,0 +1,15 @@
+#define CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct poly{
+  int koeficijent;
+  int eksponent;
+  struct poly *next;
+}ClanPolinoma;
+int main(void)
+{
+
+}
